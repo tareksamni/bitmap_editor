eec6ac356e31995446ac645e18d9e9ec9b09134b
Tarek N. Elsamni <tarek.samni@gmail.com>
2017-02-25 22:14:17 +0100

    Pixel class implementation

---
+++ app/pixel.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+class Pixel
+  DEFAULT_COLOUR = 'O'
+  InvalidColourError = Class.new(StandardError)
+
+  attr_reader :colour
+
+  def self.from_colour(colour)
+    p = new
+    p.colour = colour
+    p
+  end
+
+  def initialize
+    @colour = DEFAULT_COLOUR
+  end
+
+  def colour=(colour)
+    invalid_colour_str = 'Colour value must be a singal Capital letter [A-Z]'
+    raise InvalidColourError, invalid_colour_str unless colour =~ /^[A-Z]$/
+    @colour = colour
+  end
+
+  def to_s
+    @colour
+  end
+end
