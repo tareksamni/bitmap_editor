df0e61294d11f864786e0827e1d657155e1822a2
Tarek N. Elsamni <tarek.samni@gmail.com>
2017-03-16 12:31:33 +0100

    Tests for Bitmap#Fill

--- spec/bitmap_spec.rb
+++ spec/bitmap_spec.rb
@@ -34,6 +34,30 @@ describe Bitmap do
       end
     end
 
+    describe '#fill' do
+      context 'using a valid (x,y)=(2,3)' do
+        it 'should set pixels region colour' do
+          bitmap.set_colour(1, 1, 'X')
+          bitmap.set_colour(2, 1, 'X')
+          bitmap.set_colour(1, 2, 'R')
+          bitmap.set_colour(2, 2, 'R')
+          bitmap.set_colour(3, 2, 'X')
+          bitmap.fill(1, 1, 'A')
+          expect(bitmap[1, 1].to_s).to eq('A')
+          expect(bitmap[2, 1].to_s).to eq('A')
+          expect(bitmap[1, 2].to_s).to eq('R')
+          expect(bitmap[2, 2].to_s).to eq('R')
+          expect(bitmap[3, 2].to_s).to eq('X')
+        end
+      end
+
+      context 'using a invalid (x,y)=(5,5)' do
+        it 'should raise error' do
+          expect { bitmap.fill(5, 5, 'X') }.to raise_error(Bitmap::OutOfImageCoordinatesError)
+        end
+      end
+    end
+
     describe '#set_vertical_colour_range' do
       context 'with valid range' do
         it 'should set color in y range' do
